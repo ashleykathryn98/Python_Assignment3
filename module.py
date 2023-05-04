ee95417eb34516706ca9f1a982a0d58ff3150ba5
ashleykathryn98 <130531429+ashleykathryn98@users.noreply.github.com>
2023-05-04 13:38:21 -0700

    Add files via upload

---
+++ module.py
@@ -0,0 +1,6 @@
+print("What is the square footage of your house?")
+
+def getsquare(number):
+    number = input("\nEnter length and width of your house. ")
+    return number ** 2
+print(getsquare())
